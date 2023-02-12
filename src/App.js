7118a2ebd66a99c66bb3b6a63660b22d429dc221
KseniiaOldfield <kseniiaoldfield@gmail.com>
2023-02-12 16:04:46 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState(0);
+  const {id, country, image} = data[countries];
+  const backCountry = () => {
+    setCountries((countries => {
+    countries --;
+    if (countries < 0) {
+      return data.length -1;
+    }
+        return countries;
+    }))
+  }
+    const nextCountry = () => {
+      setCountries((countries => {
+        countries ++;
+        if (countries > data.length -1){
+          countries = 0;
+        }
+        return countries;
+      }))
+    } 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <div className="container">
+        <h1>MY GOALS FOR THIS YEAR</h1>
+      </div>
+      <div className="container">
+          <h2>{id}. {country}</h2>
+        </div>
+      <div className='box'>
+        <div className='btn'>
+          <button onClick={backCountry}> Back </button>
+        </div>
+        <div className="container1">
+          <img className='photo' src={image} width="500px" alt='country'/>
+        </div>
+        <div className='btn'>
+          <button onClick={nextCountry}> Next </button>
+        </div>
+      </div>
+    </div>);
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const data = [
+    {
+        id: 1,
+        country: "Thailand",
+        image: "https://images.pexels.com/photos/248082/pexels-photo-248082.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    {
+        id: 2,
+        country: "New York",
+        image: "https://images.pexels.com/photos/1239162/pexels-photo-1239162.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    {
+        id: 3,
+        country: "Spain",
+        image: "https://images.pexels.com/photos/1388030/pexels-photo-1388030.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    {
+        id: 4,
+        country: "Maldives",
+        image: "https://images.pexels.com/photos/1483053/pexels-photo-1483053.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    {
+        id: 5,
+        country: "France",
+        image: "https://images.pexels.com/photos/12485861/pexels-photo-12485861.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+]
