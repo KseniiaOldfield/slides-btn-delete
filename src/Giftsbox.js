ed436216d535049d7eaf8e11dfb842fea8acc5f1
KseniiaOldfield <kseniiaoldfield@gmail.com>
2023-02-23 18:10:14 +0000

    css

--- src/Giftsbox.js
+++ src/Giftsbox.js
@@ -7,13 +7,13 @@ function Giftsbox() {
   return (
     <div>
       <div className='container'>
-        <h1>List of {gifts.length} gifts</h1>
+        <h1>List of {gifts.length} goals</h1>
       </div>
+      <div className='flexstart'>
       {gifts.map((element=>{
         const {id, gift, image} = element;
         
         const removeGift = (id) => {
-          console.log(id)
           let newGifts = gifts.filter(gift => gift.id !== id);
           console.log(newGifts);
           setGifts(newGifts)
@@ -24,12 +24,13 @@ function Giftsbox() {
             <div className='item'>
                 <h2>{id}-{gift}</h2>
                 <img src={image}  alt= "gift" width="200px"/>
-                <button onClick={() => removeGift(id)}>Delete</button>
+                <button className='del' onClick={() => removeGift(id)}>Delete</button>
             </div>
         </div>
     </div>
         )
     }))}
+    </div>
         <div className='container'>
             <button onClick={() => setGifts([])}>Delete all</button>
         </div>
